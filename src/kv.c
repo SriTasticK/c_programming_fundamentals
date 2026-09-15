81aed4730fa7fd9af55a8b7d97ccde7fea456e21
Srinivasan Arumugam <srini9665@gmail.com>
2026-09-15 19:38:57 +0530

    kv_free type void instead of int

--- src/kv.c
+++ src/kv.c
@@ -15,8 +15,8 @@ size_t hash(char *val, int capacity) {
   return hash % capacity;
 }
 
-int kv_free(kv_t *db) {
-  if (!db) return -1;
+void kv_free(kv_t *db) {
+  if (!db) return ;
   for (int i = 0; i < db->capacity; i++) {
     kv_entry_t *entry = &db->entries[i];
     if (entry->key && entry->key != (void *)TOMBSTONE) {
@@ -29,7 +29,6 @@ int kv_free(kv_t *db) {
   }
   free(db->entries);
   free(db);
-  return 0;
 }
 
 int kv_delete(kv_t *db, char *key) {
